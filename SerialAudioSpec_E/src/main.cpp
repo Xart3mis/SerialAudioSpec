23c0574caeb46076ffab31119b91b0a7652be7c7
Xart3misX <xart3misx@protonmail.com>
2021-12-09 01:21:52 +0200

    serial monitor flags and code formatting

--- SerialAudioSpec_E/src/main.cpp
+++ SerialAudioSpec_E/src/main.cpp
@@ -11,51 +11,43 @@ char initial_[3] = {0};
 int temp_offset = 0;
 int _offset = 0;
 
-//offset must be >= 0
-//size of initial must be == 3
-//size of _dest must be >= size + offset
-int receive_serial_block(float *_dest, char *initial, int size, int offset, bool _cont = 1);
+// offset must be >= 0
+// size of initial must be == 3
+// size of _dest must be >= size + offset
+int receive_serial_block(float *_dest, char *initial, int size, int offset,
+                         bool _cont = 1);
 void print_and_test(const int flashled_index, const float flashled_value);
 void receive_serial_chunk();
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   Serial.setTimeout(15);
   pinMode(PC13, OUTPUT);
 }
 
-void loop()
-{
+void loop() {
   receive_serial_chunk();
   print_and_test(200, 6969);
 }
 
-void receive_serial_chunk()
-{
+void receive_serial_chunk() {
   temp_offset = receive_serial_block(audio_data, initial_, 10, _offset);
 
   char mode = initial_[strlen(initial_) - 1];
 
-  if (mode != 'E')
-  {
+  if (mode != 'E') {
     _offset += temp_offset;
-  }
-  else if (mode == 'R')
-  {
+  } else if (mode == 'R') {
     _offset = 0;
     temp_offset = 0;
-  }
-  else
-  {
+  } else {
     _offset = 0;
     temp_offset = 0;
   }
 }
 
-int receive_serial_block(float *_dest, char *initial, const int size, const int offset, bool _cont)
-{
-
+int receive_serial_block(float *_dest, char *initial, const int size,
+                         const int offset, bool _cont) {
   char buffer[192] = {'\0'};
 
 SerialAV:
@@ -64,25 +56,20 @@ SerialAV:
   else
     goto SerialAV;
 
-  if (buffer[0] != '@')
-  {
+  if (buffer[0] != '@') {
     return 0;
   }
 
-  for (int i = 0; i < 3; i++)
-  {
+  for (int i = 0; i < 3; i++) {
     initial[i] = buffer[i + 1];
   }
 
-  for (int i = 0; i < 192 - 4; i++)
-  {
+  for (int i = 0; i < 192 - 4; i++) {
     buffer[i] = buffer[i + 4];
   }
 
-  for (int i = 0; i < 2; i++)
-  {
-    if (initial[i] == '_')
-    {
+  for (int i = 0; i < 2; i++) {
+    if (initial[i] == '_') {
       initial[i] = ' ';
     }
   }
@@ -90,10 +77,8 @@ SerialAV:
   char *p = initial;
   int len = strlen(p);
 
-  while (isspace(p[len - 1]))
-    p[--len] = 0;
-  while (*p && isspace(*p))
-    ++p, --len;
+  while (isspace(p[len - 1])) p[--len] = 0;
+  while (*p && isspace(*p)) ++p, --len;
 
   memmove(initial, p, len + 1);
 
@@ -105,8 +90,7 @@ SerialAV:
   _dest[0 + offset] = atof(pch);
 
   int i = 1;
-  while (pch != NULL && i <= size)
-  {
+  while (pch != NULL && i <= size) {
     pch = strtok(NULL, delim);
     _dest[i + offset] = atof(pch);
     i++;
@@ -115,8 +99,7 @@ SerialAV:
   return i - 1;
 }
 
-void print_and_test(const int flashled_index, const float flashled_value)
-{
+void print_and_test(const int flashled_index, const float flashled_value) {
 #ifdef DEBUG
 #ifdef INFO_DBG
   Serial.print("Initial String: ");
@@ -125,40 +108,29 @@ void print_and_test(const int flashled_index, const float flashled_value)
   Serial.println(initial_[strlen(initial_) - 1]);
   Serial.print("Offset: ");
   Serial.println(_offset);
-#endif // INFO_DBG
+#endif  // INFO_DBG
 #ifdef DATA_DBG
   int audio_data_len = round(sizeof(audio_data) / sizeof(audio_data[0]));
 
   Serial.print("\n{");
-  for (int i = 0; i < audio_data_len / 2; i++)
-  {
+  for (int i = 0; i < audio_data_len / 2; i++) {
     Serial.print(audio_data[i]);
-    if (i < (audio_data_len / 2) - 1)
-      Serial.print(", ");
+    if (i < (audio_data_len / 2) - 1) Serial.print(", ");
   }
   Serial.println("}\n");
-#endif // DATA_DBG
+#endif  // DATA_DBG
 #ifdef FLASH_DBG
   int flash_time = 3000;
 
-  unsigned long long c_millis = millis();
-  unsigned long long p_millis = 0;
-
-  if (audio_data[flashled_index] == flashled_value)
-  {
-    for (int i = 0; i < flash_time / 50; i++)
-    {
-      // if (c_millis - p_millis >= 50)
-      // {
-      //   p_millis = c_millis;
+  if (audio_data[flashled_index] == flashled_value) {
+    for (int i = 0; i < flash_time / 50; i++) {
       digitalWrite(PC13, digitalRead(PC13) ^ 1);
       delay(50);
-      // }
     }
     digitalWrite(PC13, 0);
   }
-#endif // FLASH_DBG
-#endif // DEBUG
+#endif  // FLASH_DBG
+#endif  // DEBUG
 }
 // #include "Seriallib.h"
 // #include "Arduino.h"
